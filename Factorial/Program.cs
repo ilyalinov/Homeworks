4e2bfda76dde91ba09a20e9279bc6c458c6fcd34
Ilya Lyalinov <ilya.lyalinov@gmail.com>
2015-02-17 20:13:56 +0300

    Working factorial
    Signed-off-by: Ilya Lyalinov <ilya.lyalinov@gmail.com>

---
+++ Factorial/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static int Factorial(int number)
+        {
+            int result = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                result = result * i;
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your number >= 1 below: \n");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Result: {0}\n", Factorial(number));
+        }
+    }
+}
