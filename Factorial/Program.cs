c6b726ca58fc5d06cadbe3b9524b5d85b6369ab9
Ilya Lyalinov <ilya.lyalinov@gmail.com>
2015-02-17 22:17:44 +0300

    Changed included namespaces
    Signed-off-by: Ilya Lyalinov <ilya.lyalinov@gmail.com>

--- Factorial/Program.cs
+++ Factorial/Program.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Factorial
 {
     class Program
     {
-        static int Factorial(int number)
+        private static int Factorial(int number)
         {
             int result = 1;
             for (int i = 1; i <= number; i++)
