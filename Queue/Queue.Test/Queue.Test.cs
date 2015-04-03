d1beb1fd4367da2d65750cba55d25d4d0c4ce119
Ilya Lyalinov <ilya.lyalinov@gmail.com>
2015-04-03 15:21:48 +0300

    Queue
    Signed-off-by: Ilya Lyalinov <ilya.lyalinov@gmail.com>

---
+++ Queue/Queue.Test/Queue.Test.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QueueNamespace;
+
+namespace Queue.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public QueueClass queue = null;
+
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            QueueClass queue = new QueueClass();
+        }
+
+        [TestMethod]
+        public void EnqueueDequeueTest()
+        {
+            queue.Enqueue(1, 3);
+            Assert.AreEqual(queue.Dequeue(), 1);
+        }
+
+        [TestMethod]
+        public void SizeTest()
+        {
+            queue.Enqueue(1, 1);
+            Assert.AreEqual(1, queue.Size);
+        }
+
+        [ExpectedException(typeof(System.NullReferenceException))]
+        [TestMethod]
+        public void ExceptionTest()
+        {
+            queue.Dequeue();
+        }
+    }
+}
