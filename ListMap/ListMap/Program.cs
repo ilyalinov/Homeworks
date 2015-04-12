3b212c4d4b4c38169d7e6a270423ba888f700e5c
Ilya Lyalinov <ilya.lyalinov@gmail.com>
2015-04-12 20:39:14 +0300

    Working function Map with tests
    Signed-off-by: Ilya Lyalinov <ilya.lyalinov@gmail.com>

---
+++ ListMap/ListMap/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListMap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List list = new List();
+            list.Insert(2);
+            var modifier = new MapClass.Modifier(MapClass.ListElementModifier);
+            MapClass.Map(list, modifier);
+            list.Print();
+        }
+    }
+}
