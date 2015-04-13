ba7d37bca984201e58e955715137e31728494e1d
Ilya Lyalinov <ilya.lyalinov@gmail.com>
2015-04-13 19:40:53 +0300

    Working cursor control
    Signed-off-by: Ilya Lyalinov <ilya.lyalinov@gmail.com>

---
+++ CursorControl/CursorConsole.Test/CursorConsole.Test.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CursorControlNamespace;
+
+namespace CursorConsole.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            var eventLoop = new EventLoop();
+            var cursorControl = new Cursor();
+
+            eventLoop.RightHandler += new System.Action(cursorControl.OnRight);
+            eventLoop.LeftHandler += new System.Action(cursorControl.OnLeft);
+            eventLoop.UpHandler += new System.Action(cursorControl.Up);
+            eventLoop.DownHandler += new System.Action(cursorControl.Down);
+
+            //eventLoop.Run();
+        }
+
+        [TestMethod]
+        public void UpTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void UpDownTest()
+        {
+            cursorControl.OnLeft();
+            cursorControl.OnRight();
+        }
+
+        [TestMethod]
+        public void OnRightLeftTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void OnLeftTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void ExceptionTest()
+        {
+
+        }
+    }
+}
---
+++ CursorControl/CursorControl/CursorControl.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Threading;
+
+namespace CursorControlNamespace
+{
+    /// <summary>
+    /// Hadler class for cursor
+    /// </summary>
+    public class Cursor
+    {
+        public void OnLeft()
+        {
+            try
+            {
+                Console.CursorLeft -= 1;
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.Clear();
+                Console.WriteLine("Can't move further (sleep for 3 seconds, then cleaning the console)");
+                Thread.Sleep(3000);
+                Console.Clear();
+            }
+        }
+
+        /// <summary>
+        /// Cursor right
+        /// </summary>
+        public void OnRight()
+        {
+            try
+            {
+                Console.CursorLeft += 1;
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.Clear();
+                Console.WriteLine("Can't move further (sleep for 3 seconds, then cleaning the console)");
+                Thread.Sleep(3000);
+                Console.Clear();
+            }
+        }
+
+        /// <summary>
+        /// Cursor up
+        /// </summary>
+        public void Up()
+        {
+            try
+            {
+                Console.CursorTop -= 1;
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.Clear();
+                Console.WriteLine("Can't move further (sleep for 3 seconds, then cleaning the console)");
+                Thread.Sleep(3000);
+                Console.Clear();
+            }
+        }
+
+        /// <summary>
+        /// Cursor down
+        /// </summary>
+        public void Down()
+        {
+            try
+            {
+                Console.CursorTop += 1;
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.Clear();
+                Console.WriteLine("Can't move further (sleep for 3 seconds, then cleaning the console)");
+                Thread.Sleep(3000);
+                Console.Clear();
+            }
+        }
+    }
+}
