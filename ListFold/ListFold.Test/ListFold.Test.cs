679441bc912e776dcb989d3e1961ddaabc0cc29a
Ilya Lyalinov <ilya.lyalinov@gmail.com>
2015-04-19 18:13:34 +0300

    Working list fold
    Signed-off-by: Ilya Lyalinov <ilya.lyalinov@gmail.com>

---
+++ ListFold/ListFold.Test/ListFold.Test.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ListFold;
+
+namespace ListFold.Test
+{
+    [TestClass]
+    public class ListFoldTest
+    {
+        public List list = null;
+        public FoldClass.UpdateValue updateValue = null;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            list = new List();
+            updateValue = new FoldClass.UpdateValue(FoldClass.Sum);
+            for (int i = 0; i <= 10; i++)
+            {
+                list.Insert(i);
+            }
+        }
+
+        /// <summary>
+        /// Counting sum 1 + ... + 10
+        /// </summary>
+        [TestMethod]
+        public void FoldTest()
+        {
+            Assert.AreEqual(FoldClass.Fold(list, 0, updateValue), 10 * 11 / 2);
+        }
+    }
+}
---
+++ ListFold/ListFold/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListFold
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int initialValue = 0;
+            var list = new List();
+            list.Insert(1);
+            list.Insert(2);
+            var updateValue = new FoldClass.UpdateValue(FoldClass.Sum);
+            Console.WriteLine(FoldClass.Fold(list, initialValue, updateValue));
+        }
+    }
+}
