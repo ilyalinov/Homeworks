2e44bc90c0e5839ad50015a2a36daa072688137d
Ilya Lyalinov <ilya.lyalinov@gmail.com>
2015-05-14 20:46:00 +0300

    Working tree calculator. Added auto properties, working spaces removal.

--- TreeCalculator/TreeCalculator/BinaryTree.cs
+++ TreeCalculator/TreeCalculator/BinaryTree.cs
@@ -12,54 +12,10 @@ namespace TreeCalculator
         /// </summary>
         private abstract class TreeElement
         {
-            protected string m_Key = null;
-            protected TreeElement right = null;
-            protected TreeElement left = null;
+            public string Key { get; set; }
+            public TreeElement Right { get; set; }
+            public TreeElement Left { get; set; }
 
-            /// <summary>
-            /// Get and set tree element key
-            /// </summary>
-            public string Key
-            {
-                get
-                {
-                    return this.m_Key;
-                }
-                set
-                {
-                    this.m_Key = value;
-                }
-            }
-
-            /// <summary>
-            /// get and set right child
-            /// </summary>
-            public TreeElement Right
-            {
-                get 
-                {
-                    return this.right;
-                }
-                set
-                {
-                    this.right = value;
-                }
-            }
-
-            /// <summary>
-            /// Get and set left child
-            /// </summary>
-            public TreeElement Left
-            {
-                get
-                {
-                    return this.left;
-                }
-                set
-                {
-                    this.left = value;
-                }
-            }
 
             /// <summary>
             /// Construct tree element by its key
@@ -67,7 +23,7 @@ namespace TreeCalculator
             /// <param name="key"></param>
             public TreeElement(string key)
             {
-                this.m_Key = key;
+                this.Key = key;
             }
 
             /// <summary>
@@ -89,7 +45,7 @@ namespace TreeCalculator
                 {
                     this.Right.Print();
                 }
-                Console.Write(m_Key + " ");
+                Console.Write(Key + " ");
             }
         }
 
@@ -98,23 +54,22 @@ namespace TreeCalculator
         /// </summary>
         private class Operation : TreeElement
         {
-            public Operation(string key)
-                : base(key)
+            public Operation(string key) : base(key)
             {
             }
 
             public override int Count()
             {
-                switch (this.m_Key)
+                switch (this.Key)
                 {
                     case "+":
-                        return this.left.Count() + this.right.Count();
+                        return this.Left.Count() + this.Right.Count();
                     case "-":
-                        return this.left.Count() - this.right.Count();
+                        return this.Left.Count() - this.Right.Count();
                     case "*":
-                        return this.left.Count() * this.right.Count();
+                        return this.Left.Count() * this.Right.Count();
                     case "/":
-                        return this.left.Count() / this.right.Count();
+                        return this.Left.Count() / this.Right.Count();
                     default:
                         throw new Exception("Something is very wrong");
                 }
@@ -133,7 +88,7 @@ namespace TreeCalculator
 
             public override int Count()
             {
-                return Convert.ToInt32(this.m_Key);
+                return Convert.ToInt32(this.Key);
             }
         }
 
@@ -153,21 +108,21 @@ namespace TreeCalculator
         public BinaryTree(string expression)
         {
             this.head = new Operation(expression);
-            this.Method(this.head);
+            this.CreateTree(this.head);
         }
 
         /// <summary>
         /// Write operation in the given tree element and the operands in the children
         /// </summary>
         /// <param name="treeElement"> given tree element </param>
-        private void Method(TreeElement treeElement)
+        private void CreateTree(TreeElement treeElement)
         {
             string[] dividedExpression = ParseString.DivideOperand(treeElement.Key);
             treeElement.Key = dividedExpression[0];
             if (ParseString.IsOperandComplex(dividedExpression[1]))
             {
                 treeElement.Left = new Operation(dividedExpression[1]);
-                this.Method(treeElement.Left);
+                this.CreateTree(treeElement.Left);
             }
             else
             {
@@ -176,7 +131,7 @@ namespace TreeCalculator
             if (ParseString.IsOperandComplex(dividedExpression[2]))
             {
                 treeElement.Right = new Operation(dividedExpression[2]);
-                this.Method(treeElement.Right);
+                this.CreateTree(treeElement.Right);
             }
             else
             {
