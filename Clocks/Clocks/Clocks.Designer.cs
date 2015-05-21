96f83feb553daca7b137ab92613d8a8dd49e8238
Ilya Lyalinov <ilya.lyalinov@gmail.com>
2015-05-21 19:04:12 +0300

    Working hw 6.2. Clocks with timer.

---
+++ Clocks/Clocks/Clocks.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Clocks
+{
+    partial class Clocks
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.secondsTextBox = new System.Windows.Forms.RichTextBox();
+            this.minutesTextBox = new System.Windows.Forms.RichTextBox();
+            this.hoursTextBox = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tableLayoutPanel1.ColumnCount = 5;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 59F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 59F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 234F));
+            this.tableLayoutPanel1.Controls.Add(this.textBox2, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.secondsTextBox, 4, 0);
+            this.tableLayoutPanel1.Controls.Add(this.minutesTextBox, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.hoursTextBox, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(50, 50);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 110F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(340, 110);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Enabled = false;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(141, 3);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(14, 100);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = ":";
+            // 
+            // secondsTextBox
+            // 
+            this.secondsTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.secondsTextBox.Enabled = false;
+            this.secondsTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.secondsTextBox.Location = new System.Drawing.Point(161, 3);
+            this.secondsTextBox.Name = "secondsTextBox";
+            this.secondsTextBox.ReadOnly = true;
+            this.secondsTextBox.Size = new System.Drawing.Size(50, 100);
+            this.secondsTextBox.TabIndex = 1;
+            this.secondsTextBox.Text = "";
+            // 
+            // minutesTextBox
+            // 
+            this.minutesTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.minutesTextBox.Enabled = false;
+            this.minutesTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.minutesTextBox.Location = new System.Drawing.Point(82, 3);
+            this.minutesTextBox.Name = "minutesTextBox";
+            this.minutesTextBox.ReadOnly = true;
+            this.minutesTextBox.Size = new System.Drawing.Size(50, 100);
+            this.minutesTextBox.TabIndex = 0;
+            this.minutesTextBox.Text = "";
+            // 
+            // hoursTextBox
+            // 
+            this.hoursTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.hoursTextBox.Enabled = false;
+            this.hoursTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.hoursTextBox.Location = new System.Drawing.Point(3, 3);
+            this.hoursTextBox.Name = "hoursTextBox";
+            this.hoursTextBox.ReadOnly = true;
+            this.hoursTextBox.Size = new System.Drawing.Size(50, 100);
+            this.hoursTextBox.TabIndex = 1;
+            this.hoursTextBox.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(62, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(14, 100);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = ":";
+            // 
+            // Clocks
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(542, 235);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MinimumSize = new System.Drawing.Size(500, 200);
+            this.Name = "Clocks";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.RichTextBox secondsTextBox;
+        private System.Windows.Forms.RichTextBox hoursTextBox;
+        private System.Windows.Forms.RichTextBox minutesTextBox;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+
+    }
+}
+
---
+++ Clocks/Clocks/Clocks.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clocks
+{
+    public partial class Clocks : Form
+    {
+        public Clocks()
+        {
+            InitializeComponent();
+        }
+
+        private string Add0(string buffer)
+        {
+            if (Convert.ToInt32(buffer) < 10)
+            {
+                buffer = "0" + buffer;
+            }
+            return buffer;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var time = DateTime.Now;
+            string hours = Convert.ToString(time.Hour);
+            string minutes = Convert.ToString(time.Minute);
+            string seconds = Convert.ToString(time.Second);
+            hours = Add0(hours);
+            minutes = Add0(minutes);
+            seconds = Add0(seconds);
+
+            this.hoursTextBox.Text = hours;
+            this.minutesTextBox.Text = minutes;
+            this.secondsTextBox.Text = seconds;
+        }
+    }
+}
