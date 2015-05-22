521878f54db5ba2b6e837792e0c2a72faac5f899
Ilya Lyalinov <ilya.lyalinov@gmail.com>
2015-05-22 15:34:42 +0300

    Final test. Tree iterator

---
+++ Iterator/Iterator.Test/IteratorTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IteratorNamespace;
+using System.Collections.Generic;
+
+namespace Iterator.Test
+{
+    [TestClass]
+    public class IteratorTest
+    {
+        private IteratorClass iterator;
+
+        private BinaryTree tree;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            List<int> list = new List<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(i);
+            }
+            tree = new BinaryTree(list);
+            iterator = new IteratorClass(tree);
+        }
+
+        [TestMethod]
+        public void IteratorTest1()
+        {
+            int i = 0;
+            while(!iterator.IsEmpty())
+            {
+                Assert.AreEqual(i, iterator.Next().Key);
+                i++;
+            }
+        }
+    }
+}
