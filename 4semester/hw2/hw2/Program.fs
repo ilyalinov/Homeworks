d284981e9aaba50a4d871ae63afba3fe21181c32
Ilya Lyalinov <ilya.lyalinov@gmail.com>
2016-03-31 23:58:22 +0300

    hw2 tasks 1-4

--- 4semester/hw2/hw2/Program.fs
+++ 4semester/hw2/hw2/Program.fs
@@ -27,3 +27,43 @@ let testList = [1..100]
 printfn "%i" <| findPosition testList 10
 
 // task 3
+let listReverse list = 
+    let rec listReverseUtil acc list =
+        match list with
+        | head :: tail -> listReverseUtil (head :: acc) tail
+        | [] -> acc
+    listReverseUtil [] list
+
+let isPalindrom s = 
+    s
+    |> Seq.toList
+    |> listReverse
+    |> (=) <| Seq.toList s
+
+printfn "%b" <| isPalindrom "123321"
+printfn "%b" <| isPalindrom "1234445321"
+
+// task 4
+let rec quicksort list = 
+    match list with
+    | [] -> []
+    | head :: tail -> 
+        let left = 
+            tail |> List.filter (fun x -> x <= head) |> quicksort
+        let right = 
+            tail |> List.filter (fun x -> x > head) |> quicksort
+        left @ [head] @ right
+
+let rec checkSortedList list = 
+    match list with
+    | [] -> true
+    | head :: tail when List.length tail > 0 -> 
+        if head = tail.Head then false
+        else checkSortedList tail 
+    | _ -> true
+
+let areAllElemsDifferent list = 
+    list |> quicksort |> checkSortedList
+
+printfn "%b" <| areAllElemsDifferent [1;3;2;0]
+printfn "%b" <| areAllElemsDifferent [1;3;2;0;2]
