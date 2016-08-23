0ab49813395a7131d4ef61cfd559921aef958c5c
Ilya Lyalinov <ilya.lyalinov@gmail.com>
2016-08-23 17:22:33 +0300

    fixed simp function

--- 4semester/Differentiation/Differentiation/Program.fs
+++ 4semester/Differentiation/Differentiation/Program.fs
@@ -26,32 +26,42 @@ let rec simp (expression : Expression<float>) =
     match expression with
     | Const(_) | Variable -> expression
     | Addition(term1, term2) ->
-        match (term1, term2) with
+        let simpTerm1 = simp term1
+        let simpTerm2 = simp term2 
+        match (simpTerm1, simpTerm2) with
         | (Const(c1), Const(c2)) -> Const(float(c1) + float(c2))
-        | _ -> Addition(simp term1, simp term2)
+        | (Const(c), term) | (term, Const(c)) when c = float(0) -> term
+        | _ -> Addition(simp simpTerm1, simp simpTerm2)
     | Substraction(term1, term2) ->
-        match (term1, term2) with
+        let simpTerm1 = simp term1
+        let simpTerm2 = simp term2
+        match (simpTerm1, simpTerm2) with
         | (Const(c1), Const(c2)) -> Const(c1 - c2)
-        | _ -> Addition(simp term1, simp term2)
+        | (term, Const(c)) when c = float(0) -> term
+        | _ -> Substraction(simpTerm1, simpTerm2)
     | Multiplication(term1, term2) ->
-        match (term1, term2) with
+        let simpTerm1 = simp term1
+        let simpTerm2 = simp term2
+        match (simpTerm1, simpTerm2) with
         | (Const(c1), Const(c2)) -> Const(c1 * c2)
         | (Const(c), term) | (term, Const(c)) ->
             match c with
             | x when x = float(1) -> simp term
             | x when x = float(0) -> Const(float(0))
-            | _ -> Multiplication(simp term1, simp term2)
-        | _ -> simp <| Multiplication(simp term1, simp term2)
-    | Division(term1, term2) -> 
-        match (term1, term2) with
+            | _ -> Multiplication(Const(c), simp term)
+        | _ -> Multiplication(simpTerm1, simpTerm2)
+    | Division(term1, term2) ->
+        let simpTerm1 = simp term1
+        let simpTerm2 = simp term2 
+        match (simpTerm1, simpTerm2) with
         | (Const(c1), Const(c2)) -> Const(c1 / c2)
         | (Const(c), _) when c = float(0) -> Const(float(0))
         | (term, Const(c)) ->
             match c with 
             | x when x = float(0) -> failwith "Division by zero"
             | x when x = float(1) -> simp term
-            | _ -> Division(simp term1, simp term2)
-        | _ -> Division(simp term1, simp term2)
+            | _ -> Division(simp term, Const(c))
+        | _ -> simp <| Division(simpTerm1, simpTerm2)
     
 //let testExpression = 
 //    Multiplication(
@@ -59,6 +69,10 @@ let rec simp (expression : Expression<float>) =
 //        Addition(Const(2.5), Variable)
 //    )
 
-let testExpression = Multiplication(Variable, Variable)
+//let testExpression = Multiplication(Substraction(Const 1.0, Const 1.0), Variable)
+
+//let testExpression = Addition(Const(0.0), Variable)
+
+//let testExpression = Addition(Substraction(Multiplication(Const(2.5), Variable), Multiplication(Const(3.5), Variable)), Multiplication(Variable, Variable))
 
 printfn "%A" (simp (diff testExpression))
