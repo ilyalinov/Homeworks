231d04ec408850066b2ddc957020c51399a563e5
Ilya Lyalinov <ilya.lyalinov@gmail.com>
2016-08-26 06:27:45 +0300

    hw 8.1

---
+++ 4semester/ThreadsSum/ThreadsSum/Program.fs
@@ -0,0 +1,23 @@
+﻿open System.Threading
+
+let oneMillion = Array.create 1000000 1
+let mutable flags = Array.create 100 false
+let mutable result = 0
+
+let threadBody i = 
+    for j = 1 to 10000 do
+        result <- result + oneMillion.[i * j - 1]
+    flags.[i - 1] <- true 
+
+let threads = seq {
+    for i = 1 to 100 do
+        let threadBody () = threadBody i
+        yield new Thread(threadBody)
+    }
+
+let sumArray = 
+    threads |> Seq.iter (fun x -> x.Start())
+
+    while not (Array.forall (fun x -> x) flags) do Thread.Sleep(0)
+         
+printfn "%A" result
